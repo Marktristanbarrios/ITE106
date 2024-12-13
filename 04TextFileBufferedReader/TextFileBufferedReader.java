cf578a4f6f6ef700114ac03d23726625cd4060c2
Marktristanbarrios <tristanbarrios175@gmail.com>
2024-12-13 12:46:36 +0800

    Create TextFileBufferedReader.java

---
+++ 04TextFileBufferedReader/TextFileBufferedReader.java
@@ -0,0 +1,18 @@
+
+import java.io.*;
+
+public class TextFileBufferedReader {
+
+	public static void main(String[] args) throws Exception
+	{
+	
+		File file = new File("LevyCasiano.txt");
+		
+		BufferedReader br = new BufferedReader(new FileReader(file));
+		String st;
+		while ((st = br.readLine()) != null)
+		{
+			System.out.println(st);
+		}
+	}
+}
