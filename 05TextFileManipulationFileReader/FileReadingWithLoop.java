126895df24d0b808778e5654ec1005a04962139d
Marktristanbarrios <tristanbarrios175@gmail.com>
2024-12-13 12:47:16 +0800

    Create FileReadingWithLoop.java

---
+++ 05TextFileManipulationFileReader/FileReadingWithLoop.java
@@ -0,0 +1,15 @@
+
+import java.io.File;
+import java.util.Scanner;
+public class TextFileManipulatorFileReader
+{
+	public static void main(String[] ards) throws Exception
+	{
+
+	File file = new File("LevyCasiano.txt");
+	Scanner sc = new Scanner(file);
+	
+	while (sc.hasNextLine())
+		System.out.println(sc.nextLine());
+	}
+}
